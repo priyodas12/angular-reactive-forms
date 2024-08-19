3ed15c998d8500c53a76ce6247005d3012edf56e
priyonrato.das <priyodas12@gmail.com>
2024-08-19 23:32:16 +0530

    reactive form and validations

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-card-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './card-form.component.html',
+  styleUrl: './card-form.component.css',
+})
+export class CardFormComponent implements OnInit {
+  cardForm = new FormGroup({
+    fullName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(64),
+    ]),
+  });
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+  }
+}
